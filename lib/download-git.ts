deb20dc94a3718202379f19bf55ec7fc08e83b71
Brendan Forster <brendan@github.com>
2016-08-26 10:40:19 +1000

    rewriting script to use temporary cache and verify checksum

--- lib/download-git.ts
+++ lib/download-git.ts
@@ -6,6 +6,7 @@ const tmpdir = require('os-tmpdir')
 import * as mkdirp from 'mkdirp'
 import * as path from 'path'
 import * as fs from 'fs'
+import * as checksum from 'checksum'
 
 const baseUrl = process.env.NPM_CONFIG_ELECTRON_MIRROR ||
   process.env.npm_config_electron_mirror ||
@@ -15,7 +16,7 @@ const baseUrl = process.env.NPM_CONFIG_ELECTRON_MIRROR ||
 
 const config = {
   baseUrl: baseUrl,
-  outputPath: path.join(__dirname, 'git'),
+  outputPath: path.join(__dirname, '..', 'git'),
   version: '2.9.3',
   source: '',
   checksum: '',
@@ -36,6 +37,8 @@ if (process.platform === 'darwin') {
   config.checksum = '17e40cb149ce6a348c8e8bbe7f1c1fff00f82882f0e57f32d60ea5c26feeef98'
 }
 
+const fullUrl = config.source
+
 function handleError (url: string, error: Error) {
   if (!error) {
     return
@@ -46,49 +49,63 @@ function handleError (url: string, error: Error) {
   process.exit(1)
 }
 
-function unzip(file: Buffer, callback: any) {
-  decompress(file, config.outputPath).then(() => {
+function unzip(path: string, callback: any) {
+  console.log(`unzipping to ${config.outputPath}`)
+  decompress(path, config.outputPath).then(() => {
     console.log('done!')
   })
 }
 
-mkdirp(config.outputPath, async function (error) {
-  const fullUrl = config.source
+const dir = tmpdir()
+const temporaryFile = path.join(dir, config.fileName)
+
+const verifyFile = function(file: string, callback: (valid: boolean) => void) {
+  checksum.file(file, { algorithm: 'sha256' }, (error: Error, hash: string) => {
+    callback(hash === config.checksum)
+  })
+}
+
+const unpackFile = function (file: string) {
+  console.log(`unzipping...`)
+
+  unzip(file, function (error: Error) {
+    if (error) {
+      return handleError(fullUrl, error)
+    }
+  })
+}
+
+const downloadCallback = function (error: Error, response: any, body: any) {
 
   if (error) {
     return handleError(fullUrl, error)
   }
 
-  console.log(`Download Git from: ${fullUrl}`)
+  if (response.statusCode !== 200) {
+    return handleError(fullUrl, Error(`Non-200 response (${response.statusCode})`))
+  }
 
-  const callback = function (error: Error, response: any, body: any) {
-
-    console.log(`done?`)
-
+  fs.createWriteStream(temporaryFile).write(body, function(error: Error) {
     if (error) {
       return handleError(fullUrl, error)
     }
 
-    if (response.statusCode !== 200) {
-      return handleError(fullUrl, Error(`Non-200 response (${response.statusCode})`))
-    }
-
-    const dir = tmpdir()
-    const file = path.join(dir, config.fileName)
-
-    fs.createWriteStream(file).write(body, function(error: Error) {
-      // TODO: checksum bytes received
-      console.log(`unzipping`)
-
-      unzip(body, function (error: Error) {
-        if (error) {
-          return handleError(fullUrl, error)
-        }
-      })
+    verifyFile(temporaryFile, valid => {
+      if (valid) {
+        console.log('file valid. unpacking...')
+        unpackFile(temporaryFile)
+      } else {
+        console.log('file not valid. aborting...')
+        process.exit(1)
+      }
     })
-  }
+  })
+}
 
-  const req = request.get(fullUrl, { encoding: null }, callback)
+const downloadAndUnpack = () => {
+  console.log(`Download Git from: ${fullUrl}`)
+
+  const req = request.get(fullUrl, { encoding: null }, downloadCallback)
 
   req.on('response', function(res: any) {
     const len = parseInt(res.headers['content-length'], 10);
@@ -97,7 +114,7 @@ mkdirp(config.outputPath, async function (error) {
     const bar = new ProgressBar('Downloading Git [:bar] :percent :etas', {
       complete: '=',
       incomplete: ' ',
-      width: 20,
+      width: 50,
       total: len
     });
 
@@ -109,5 +126,30 @@ mkdirp(config.outputPath, async function (error) {
       console.log('\n');
     });
   })
+}
 
+mkdirp(config.outputPath, async function (error) {
+  if (error) {
+    return handleError(fullUrl, error)
+  }
+
+  console.log(`checking for file ${temporaryFile}...`)
+
+  if (fs.existsSync(temporaryFile)) {
+    console.log('file exists. verifying...')
+    verifyFile(temporaryFile, valid => {
+      if (valid) {
+        console.log('file valid. unpacking...')
+        unpackFile(temporaryFile)
+      } else {
+        console.log('file not valid. try again...')
+        // downloadAndUnpack()
+      }
+    })
+    return
+  }
+
+  console.log(`file does not exist. downloading...`)
+
+  downloadAndUnpack()
 })
