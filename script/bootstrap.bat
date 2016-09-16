46ff487bb3f3a7cc1868b504b1c7dd44bce7f76e
joshaber <joshaber@gmail.com>
2016-09-16 16:33:21 -0400

    Install rimraf up front too

--- script/bootstrap.bat
+++ script/bootstrap.bat
@@ -2,6 +2,7 @@ ECHO OFF
 
 :: install pre-requisites so everything can be built
 CALL npm install typescript@rc -g
+CALL npm install rimraf
 CALL npm install tslint
 
 :: build the artefacts needed so that install works
