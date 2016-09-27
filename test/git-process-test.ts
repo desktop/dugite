016c59e1f709c4a3ff30956b39c7949cebf323d7
joshaber <joshaber@gmail.com>
2016-09-27 16:25:48 -0400

    Sanity check error parsing

--- test/git-process-test.ts
+++ test/git-process-test.ts
@@ -1,11 +1,16 @@
 import * as chai from 'chai'
 const expect = chai.expect
 
-import { GitProcess } from '../lib/git-process'
+import { GitProcess, GitError } from '../lib'
 
 describe('git-process', () => {
   it('can launch git', async () => {
     const version  = await GitProcess.execWithOutput([ '--version' ], __dirname)
     expect(version.length).to.be.greaterThan(0)
+  })
+
+  it('can parse errors', () => {
+    const error = GitProcess.parseError('fatal: Authentication failed')
+    expect(error).to.equal(GitError.SSHAuthenticationFailed)
   })
 })
