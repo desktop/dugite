5f1346580730a471b13bae5ab8c5bc9294d0a366
joshaber <joshaber@gmail.com>
2016-10-28 14:49:11 -0400

    Consolidate error tests

--- test/git-process-test.ts
+++ test/git-process-test.ts
@@ -19,21 +19,23 @@ describe('git-process', () => {
     expect(result.exitCode).to.equal(128)
   })
 
-  it('raises error when folder does not exist', async () => {
-    const testRepoPath = path.join(temp.path(), 'desktop-does-not-exist')
+  describe('errors', () => {
+    it('raises error when folder does not exist', async () => {
+      const testRepoPath = path.join(temp.path(), 'desktop-does-not-exist')
 
-    let error: Error | null = null
-    try {
-      await GitProcess.execWithOutput([ 'show', 'HEAD' ], testRepoPath)
-    } catch (e) {
-      error = e
-    }
+      let error: Error | null = null
+      try {
+        await GitProcess.execWithOutput([ 'show', 'HEAD' ], testRepoPath)
+      } catch (e) {
+        error = e
+      }
 
-    expect(error!.message).to.equal('Unable to find path to repository on disk.')
-  })
+      expect(error!.message).to.equal('Unable to find path to repository on disk.')
+    })
 
-  it('can parse errors', () => {
-    const error = GitProcess.parseError('fatal: Authentication failed')
-    expect(error).to.equal(GitError.SSHAuthenticationFailed)
+    it('can parse errors', () => {
+      const error = GitProcess.parseError('fatal: Authentication failed')
+      expect(error).to.equal(GitError.SSHAuthenticationFailed)
+    })
   })
 })
