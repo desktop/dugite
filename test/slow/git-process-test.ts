a8c5d5b8228407ec582b4911738b28e07d235e3c
Brendan Forster <brendan@github.com>
2016-12-19 18:08:02 +1100

    cleanup and warning for these tests

--- test/slow/git-process-test.ts
+++ test/slow/git-process-test.ts
@@ -7,37 +7,48 @@ import { setupAskPass, setupNoAuth } from './auth'
 const temp = require('temp').track()
 
 describe('git-process', () => {
-    describe('clone', () => {
-      it('returns exit code and error when repository doesn\'t exist', async () => {
-        const testRepoPath = temp.mkdirSync('desktop-git-test-blank')
-        const options = {
-          env: setupNoAuth()
-        }
+  before(async () => {
+    const homeDirectory = ''
+    const config = await GitProcess.exec([ 'config', 'credential.helper' ], homeDirectory)
+    if (config.exitCode === 0) {
+      const configValue = config.stdout.trim()
+      console.warn(`WARNING:`)
+      console.warn(`You have a credential helper enabled: credential.helper=${configValue}`)
+      console.warn(`This will affect some of the test methods defined here`)
+    }
+  })
 
-        // GitHub will prompt for (and validate) credentials for non-public
-        // repositories, to prevent leakage of information.
-        // Bitbucket will not prompt for credentials, and will immediately
-        // return whether this non-public repository exists.
-        //
-        // This is an easier to way to test for the specific error than to
-        // pass live account credentials to Git.
-        const result = await GitProcess.exec([ 'clone', '--', 'https://bitbucket.org/shiftkey/testing-non-existent.git', '.'], testRepoPath, options)
-        expect(result.exitCode).to.equal(128)
-        const error = GitProcess.parseError(result.stderr)
-        expect(error).to.equal(GitError.HTTPSRepositoryNotFound)
-      })
+  describe('clone', () => {
+    it('returns exit code and error when repository doesn\'t exist', async () => {
+      const testRepoPath = temp.mkdirSync('desktop-git-test-blank')
+      const options = {
+        env: setupNoAuth()
+      }
 
-      it('returns exit code and error when repository requires credentials', async () => {
-        const testRepoPath = temp.mkdirSync('desktop-git-test-blank')
-        const options = {
-          env: setupAskPass('error', 'error')
-        }
-        const result = await GitProcess.exec([ 'clone', '--', 'https://github.com/shiftkey/repository-private.git', '.'], testRepoPath, options)
-        console.log(`stdout: ${result.stdout}`)
-        console.log(`stderr: ${result.stderr}`)
-        expect(result.exitCode).to.equal(128)
-        const error = GitProcess.parseError(result.stderr)
-        expect(error).to.equal(GitError.HTTPSAuthenticationFailed)
-      })
+      // GitHub will prompt for (and validate) credentials for non-public
+      // repositories, to prevent leakage of information.
+      // Bitbucket will not prompt for credentials, and will immediately
+      // return whether this non-public repository exists.
+      //
+      // This is an easier to way to test for the specific error than to
+      // pass live account credentials to Git.
+      const result = await GitProcess.exec([ 'clone', '--', 'https://bitbucket.org/shiftkey/testing-non-existent.git', '.'], testRepoPath, options)
+      expect(result.exitCode).to.equal(128)
+      const error = GitProcess.parseError(result.stderr)
+      expect(error).to.equal(GitError.HTTPSRepositoryNotFound)
     })
+
+    it('returns exit code and error when repository requires credentials', async () => {
+      const testRepoPath = temp.mkdirSync('desktop-git-test-blank')
+      const options = {
+        env: setupAskPass('error', 'error')
+      }
+      const result = await GitProcess.exec([ 'clone', '--', 'https://github.com/shiftkey/repository-private.git', '.'], testRepoPath, options)
+      console.log(`stdout: ${result.stdout}`)
+      console.log(`stderr: ${result.stderr}`)
+      expect(result.exitCode).to.equal(128)
+      const error = GitProcess.parseError(result.stderr)
+      expect(error).to.equal(GitError.HTTPSAuthenticationFailed)
+    })
+  })
 })
