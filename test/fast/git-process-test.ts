fcf5d3402040bdfe174ab0b6b054c6e0d2bf8217
Brendan Forster <brendan@github.com>
2016-12-19 17:33:20 +1100

    move slow tests in

--- test/fast/git-process-test.ts
+++ test/fast/git-process-test.ts
@@ -6,7 +6,7 @@ import * as fs from 'fs'
 import * as crypto from 'crypto'
 
 import { GitProcess, GitError } from '../../lib'
-import { initalize, setupAskPass } from '../helpers'
+import { initalize } from '../helpers'
 
 const temp = require('temp').track()
 
@@ -21,42 +21,6 @@ describe('git-process', () => {
       const testRepoPath = temp.mkdirSync('desktop-git-test-blank')
       const result = await GitProcess.exec([ 'show', 'HEAD' ], testRepoPath)
       expect(result.exitCode).to.equal(128)
-    })
-
-    describe('clone', () => {
-      it('returns exit code and error when repository doesn\'t exist', async () => {
-        const testRepoPath = temp.mkdirSync('desktop-git-test-blank')
-        const options = { 
-          env: {
-            // supported since Git 2.3, this is used to ensure we never interactively prompt
-            // for credentials - even as a fallback
-            GIT_TERMINAL_PROMPT: '0'
-          }
-        }
-
-        // GitHub will prompt for (and validate) credentials for non-public
-        // repositories, to prevent leakage of information.
-        // Bitbucket will not prompt for credentials, and will immediately
-        // return whether this non-public repository exists.
-        //
-        // This is an easier to way to test for the specific error than to
-        // pass live account credentials to Git.
-        const result = await GitProcess.exec([ 'clone', '--', 'https://bitbucket.org/shiftkey/testing-non-existent.git', '.'], testRepoPath, options)
-        expect(result.exitCode).to.equal(128)
-        const error = GitProcess.parseError(result.stderr)
-        expect(error).to.equal(GitError.HTTPSRepositoryNotFound)
-      })
-
-      it('returns exit code and error when repository requires credentials', async () => {
-        const testRepoPath = temp.mkdirSync('desktop-git-test-blank')
-        const options = { 
-          env: setupAskPass('error', 'error')
-        }
-        const result = await GitProcess.exec([ 'clone', '--', 'https://github.com/shiftkey/repository-private.git', '.'], testRepoPath, options)
-        expect(result.exitCode).to.equal(128)
-        const error = GitProcess.parseError(result.stderr)
-        expect(error).to.equal(GitError.HTTPSAuthenticationFailed)
-      })
     })
 
     describe('diff', () => {
---
+++ test/slow/git-process-test.ts
@@ -0,0 +1,44 @@
+const expect = chai.expect
+
+import { GitProcess, GitError } from '../../lib'
+import { setupAskPass } from '../helpers'
+
+const temp = require('temp').track()
+
+describe('git-process', () => {
+    describe('clone', () => {
+      it('returns exit code and error when repository doesn\'t exist', async () => {
+        const testRepoPath = temp.mkdirSync('desktop-git-test-blank')
+        const options = { 
+          env: {
+            // supported since Git 2.3, this is used to ensure we never interactively prompt
+            // for credentials - even as a fallback
+            GIT_TERMINAL_PROMPT: '0'
+          }
+        }
+
+        // GitHub will prompt for (and validate) credentials for non-public
+        // repositories, to prevent leakage of information.
+        // Bitbucket will not prompt for credentials, and will immediately
+        // return whether this non-public repository exists.
+        //
+        // This is an easier to way to test for the specific error than to
+        // pass live account credentials to Git.
+        const result = await GitProcess.exec([ 'clone', '--', 'https://bitbucket.org/shiftkey/testing-non-existent.git', '.'], testRepoPath, options)
+        expect(result.exitCode).to.equal(128)
+        const error = GitProcess.parseError(result.stderr)
+        expect(error).to.equal(GitError.HTTPSRepositoryNotFound)
+      })
+
+      it('returns exit code and error when repository requires credentials', async () => {
+        const testRepoPath = temp.mkdirSync('desktop-git-test-blank')
+        const options = { 
+          env: setupAskPass('error', 'error')
+        }
+        const result = await GitProcess.exec([ 'clone', '--', 'https://github.com/shiftkey/repository-private.git', '.'], testRepoPath, options)
+        expect(result.exitCode).to.equal(128)
+        const error = GitProcess.parseError(result.stderr)
+        expect(error).to.equal(GitError.HTTPSAuthenticationFailed)
+      })
+    })
+})
