83f6c0c60a08d9989fb6ff1ceaab2554c7916c60
Brendan Forster <brendan@github.com>
2016-12-22 21:12:32 +1030

    tweaking build scripts to make it more static-y

--- script/platforms/macOS.sh
+++ script/platforms/macOS.sh
@@ -2,4 +2,4 @@
 make configure
 # this becomes the output directory rather than trying to install it into $HOME
 ./configure --prefix=/Users/shiftkey/build/git/
-NO_R_TO_GCC_LINKER=1 NO_INSTALL_HARDLINKS=1 NO_FINK=1 NO_DARWIN_PORTS=1 NO_TCLTK=1 make install
+NO_R_TO_GCC_LINKER=1 NO_INSTALL_HARDLINKS=1 NO_FINK=1 NO_DARWIN_PORTS=1 NO_TCLTK=1 NO_GETTEXT=1 CC="-static" make install
