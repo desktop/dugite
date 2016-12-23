d198d71d1f67c321f88919d98f6f35aca86e18fa
Brendan Forster <brendan@github.com>
2016-12-23 12:23:31 +1030

    more robust build scripts for compiling Git

--- script/platforms/macOS.sh
+++ script/platforms/macOS.sh
@@ -1,5 +1,4 @@
 # the steps and flags used to build Git on macOS
-make configure
-# this becomes the output directory rather than trying to install it into $HOME
-./configure --prefix=/Users/shiftkey/build/git/
-NO_R_TO_GCC_LINKER=1 NO_INSTALL_HARDLINKS=1 NO_FINK=1 NO_DARWIN_PORTS=1 NO_TCLTK=1 NO_GETTEXT=1 CC="-static" make install
+rm -rf /tmp/build/git/*
+make clean
+DESTDIR='/tmp/build/git' make install prefix=/ NO_TCLTK=1 NO_GETTEXT=1 NO_DARWIN_PORTS=1 NO_CROSS_DIRECTORY_HARDLINKS=1 NO_INSTALL_HARDLINKS=1
--- script/platforms/ubuntu.sh
+++ script/platforms/ubuntu.sh
@@ -1,3 +1,2 @@
-make configure
-./configure --prefix=/home/shiftkey/build/git
-V=1 NO_R_TO_GCC_LINKER=1 NO_INSTALL_HARDLINKS=1 NO_TCLTK=1 CC="-static" make install
+make clean
+DESTDIR='/home/shiftkey/build/git' make install NO_OPENSSL=1 prefix=/ gitexecdir=/lib/git-core libexecdir=/lib/git-core mandir=/share/man htmldir=/share/doc/git/html INSTALLDIRS=vendor USE_LIBPCRE=1 SANE_TOOL_PATH= INSTALL=install TAR=tar NO_CROSS_DIRECTORY_HARDLINKS=1 NO_INSTALL_HARDLINKS=1 DEFAULT_PAGER=pager DEFAULT_EDITOR=editor V=1 CC='gcc' CFLAGS='-Wall -g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -D_FORTIFY_SOURCE=2' LDFLAGS='-Wl,-Bsymbolic-functions -Wl,-z,relro'
