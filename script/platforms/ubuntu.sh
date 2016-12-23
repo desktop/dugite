00ab4b62234ce28bd6e3362fcde52caf3f7a7cf9
Brendan Forster <brendan@github.com>
2016-12-23 15:30:19 +1030

    one more build tweak for Ubuntu

--- script/platforms/ubuntu.sh
+++ script/platforms/ubuntu.sh
@@ -1,2 +1,2 @@
 make clean
-DESTDIR='/home/shiftkey/build/git' make install prefix=/ NO_TCLTK=1 NO_GETTEXT=1 USE_LIBPCRE=1 NO_CROSS_DIRECTORY_HARDLINKS=1 NO_INSTALL_HARDLINKS=1 V=1 CC='gcc' CFLAGS='-Wall -g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -D_FORTIFY_SOURCE=2' LDFLAGS='-Wl,-Bsymbolic-functions -Wl,-z,relro'
+DESTDIR='/home/shiftkey/build/git' make install prefix=/ NO_TCLTK=1 NO_GETTEXT=1 USE_LIBPCRE=1 NO_OPENSSL=1 NO_CROSS_DIRECTORY_HARDLINKS=1 NO_INSTALL_HARDLINKS=1 V=1 CC='gcc' CFLAGS='-Wall -g -O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -D_FORTIFY_SOURCE=2' LDFLAGS='-Wl,-Bsymbolic-functions -Wl,-z,relro'
