ff42c8896f29edd6899e5a9523e1e7c66179b597
Brendan Forster <brendan@github.com>
2017-01-09 09:26:03 +1100

    added a script to test committing

---
+++ test/fast/commit.ts
@@ -0,0 +1,32 @@
+import * as chai from 'chai'
+const expect = chai.expect
+
+import { GitProcess } from '../../lib'
+import { verify } from '../helpers'
+
+import * as Fs from 'fs'
+import * as Path from 'path'
+
+const temp = require('temp').track()
+
+describe('commit', () => {
+
+  it('can commit changes', async () => {
+    const testRepoPath = temp.mkdirSync('desktop-git-test-commit')
+
+    await GitProcess.exec([ 'init' ], testRepoPath)
+
+    const readme = Path.join(testRepoPath, 'README.md')
+    Fs.writeFileSync(readme, 'HELLO WORLD!')
+
+    await GitProcess.exec([ 'add', 'README.md'], testRepoPath)
+
+    const message = 'committed the README'
+    const result = await GitProcess.exec([ 'commit', '-F', '-' ], testRepoPath,  { stdin: message })
+
+    verify(result, r => {
+      expect(r.exitCode).to.equal(0)
+      expect(r.stdout).to.contain(message)
+    })
+  })
+})
