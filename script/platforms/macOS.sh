575bb489457eb9a1094d168b29fdd6229882e5f6
Brendan Forster <brendan@github.com>
2017-01-20 12:57:26 -0800

    include symlinks explicitly when archiving

--- script/platforms/macOS.sh
+++ script/platforms/macOS.sh
@@ -6,5 +6,5 @@ make clean
 DESTDIR='/tmp/build/git' make install prefix=/ NO_PERL=1 NO_TCLTK=1 NO_GETTEXT=1 NO_DARWIN_PORTS=1 NO_INSTALL_HARDLINKS=1 MACOSX_DEPLOYMENT_TARGET=10.9
 popd
 pushd /tmp/build/git/
-zip -q -r Git-2.11.0-macOS-8.zip .
+zip -q -r -9 -y Git-2.11.0-macOS-9.zip .
 popd
