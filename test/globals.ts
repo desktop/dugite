b35751970bb9d1e7568ad26266d6bf4cbf93a12e
joshaber <joshaber@gmail.com>
2017-02-13 17:04:14 -0500

    Mocha too?

--- test/globals.ts
+++ test/globals.ts
@@ -1,3 +1,4 @@
 // This shouldn't be necessary, but without this CI fails on Windows. Seems to
 // be a bug in TS itself or ts-node.
 /// <reference path="../node_modules/@types/node/index.d.ts" />
+/// <reference path="../node_modules/@types/mocha/index.d.ts" />
