04397d5f9205ac9ccc6f3c768e1ddefd9585e12f
joshaber <joshaber@gmail.com>
2017-02-13 16:54:38 -0500

    We don't need to export NotFoundErrorCode
    We treat all ENOENTs as GitNotFoundErrorCode

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,2 @@
 export { GitProcess, IGitResult, IGitExecutionOptions } from './git-process'
-export { GitError, RepositoryDoesNotExistErrorCode, GitNotFoundErrorCode, NotFoundErrorCode } from './errors'
+export { GitError, RepositoryDoesNotExistErrorCode, GitNotFoundErrorCode } from './errors'
