9220fcd6e5442e142b720a6713d1af572e92af60
Brendan Forster <brendan@github.com>
2017-05-20 11:39:20 +1000

    bump package and validate tests

--- test/helpers.ts
+++ test/helpers.ts
@@ -2,7 +2,7 @@ import { GitProcess, IGitResult } from '../lib'
 
 // NOTE: bump these versions to the latest stable releases
 export const gitVersion = '2.12.2'
-export const gitLfsVersion = '2.0.2'
+export const gitLfsVersion = '2.1.1'
 
 const temp = require('temp').track()
 
