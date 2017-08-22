0d62c0ba1dd8a684c63e5b04afd4379bf5d65e4e
Brendan Forster <brendan@github.com>
2017-08-22 18:48:05 +1000

    the checksum for linux was off

--- script/config.js
+++ script/config.js
@@ -18,7 +18,7 @@ function getConfig() {
     config.checksum = '866aa1de21707d983427c9a8f4645169f966ab5d78f4d54de3b8b990cd83e35c'
     config.source = 'https://github.com/desktop/dugite-native/releases/download/v2.14.1-rc2/dugite-native-v2.14.1-win32-10.tar.gz'
   } else if (process.platform === 'linux') {
-    config.checksum = '866aa1de21707d983427c9a8f4645169f966ab5d78f4d54de3b8b990cd83e35c'
+    config.checksum = '534168503fc96a8981cb7eca6628b854bff54694ad7fc68a29018cc9d7110754'
     config.source = 'https://github.com/desktop/dugite-native/releases/download/v2.14.1-rc2/dugite-native-v2.14.1-ubuntu-10.tar.gz'
   }
 
