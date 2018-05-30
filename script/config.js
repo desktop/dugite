577c934f686945acb688e17bd117fecefeafd698
Markus Olsson <j.markus.olsson@gmail.com>
2018-05-30 21:30:10 +0200

    Bump to latest dugite-native
    This version include a fix for Windows which removes the http.sslCAInfo system level config variable

--- script/config.js
+++ script/config.js
@@ -13,22 +13,22 @@ function getConfig() {
   }
 
   if (process.platform === 'darwin') {
-    config.checksum = '318314be05ec63cacf733cbc232025a1a8cd75c79b7537ff6f62a858b6c3610f'
+    config.checksum = '56725f902855773bd63f727524fcf7b1d3d586000f7886abd54b77f224cb5a8d'
     config.source =
-      'https://github.com/desktop/dugite-native/releases/download/v2.17.1/dugite-native-v2.17.1-macOS.tar.gz'
+      'https://github.com/desktop/dugite-native/releases/download/v2.17.1-2/dugite-native-v2.17.1-macOS.tar.gz'
   } else if (process.platform === 'win32') {
-    config.checksum = '14079ad554a215fe707fe64dc3afa041a23d8d846817938f0bacebabe0a61267'
+    config.checksum = 'f439ce2b853dece6ae1b3d9d6861ba70184bad414445958a8020c0caf30fd787'
     config.source =
-      'https://github.com/desktop/dugite-native/releases/download/v2.17.1/dugite-native-v2.17.1-win32.tar.gz'
+      'https://github.com/desktop/dugite-native/releases/download/v2.17.1-2/dugite-native-v2.17.1-win32.tar.gz'
   } else if (process.platform === 'linux') {
     if (os.arch() === 'arm64') {
-      config.checksum = '386d63ded1066f22950a6fce2f96e32f85c6d5d93b9db7528fdbe5b694d15c2e'
+      config.checksum = '61004e33e16b310c403644fb2723b9db780094a5bacb08cc8c9e76146d7b932a'
       config.source =
-        'https://github.com/desktop/dugite-native/releases/download/v2.17.1/dugite-native-v2.17.1-arm64.tar.gz'
+        'https://github.com/desktop/dugite-native/releases/download/v2.17.1-2/dugite-native-v2.17.1-arm64.tar.gz'
     } else {
-      config.checksum = '6354310e77c02397e796275276dd119267904c9e9fb6873f4b35a2785cfd5851'
+      config.checksum = 'a3750dade1682d1805623661e006f842c6bbf9cc4e450ed161e49edeb2847a86'
       config.source =
-        'https://github.com/desktop/dugite-native/releases/download/v2.17.1/dugite-native-v2.17.1-ubuntu.tar.gz'
+        'https://github.com/desktop/dugite-native/releases/download/v2.17.1-2/dugite-native-v2.17.1-ubuntu.tar.gz'
     }
   }
 
