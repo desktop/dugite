f6b9da8369bf6cf9551d4399abcdad89b25111bf
Brendan Forster <github@brendanforster.com>
2018-08-13 11:05:31 -0300

    check for x64 or x86 arch when configuring environment in Windows

--- lib/git-environment.ts
+++ lib/git-environment.ts
@@ -57,7 +57,11 @@ function resolveGitExecPath(): string {
   ) {
     return path.join(gitDir, 'libexec', 'git-core')
   } else if (process.platform === 'win32') {
-    return path.join(gitDir, 'mingw64', 'libexec', 'git-core')
+    if (process.arch === 'x64') {
+      return path.join(gitDir, 'mingw64', 'libexec', 'git-core')
+    }
+
+    return path.join(gitDir, 'mingw32', 'libexec', 'git-core')
   }
 
   throw new Error('Git not supported on platform: ' + process.platform)
@@ -80,7 +84,11 @@ export function setupEnvironment(
   const gitDir = resolveGitDir()
 
   if (process.platform === 'win32') {
-    envPath = `${gitDir}\\mingw64\\bin;${gitDir}\\mingw64\\usr\\bin;${envPath}`
+    if (process.arch === 'x64') {
+      envPath = `${gitDir}\\mingw64\\bin;${gitDir}\\mingw64\\usr\\bin;${envPath}`
+    } else {
+      envPath = `${gitDir}\\mingw32\\bin;${gitDir}\\mingw32\\usr\\bin;${envPath}`
+    }
   }
 
   const env = Object.assign(
