2104ca426497ef6281055a8c7002f302a5bc2bf2
Brendan Forster <github@brendanforster.com>
2018-11-05 21:05:50 -0400

    ugh, whatever

--- jest.slow.config.js
+++ jest.slow.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/lib/', '<rootDir>/test/'],
   testMatch: ['**/test/slow/**/*-test.ts'],
-  setupTestFrameworkScriptFile: ['<rootDir>/test/slow-setup.ts'],
+  setupTestFrameworkScriptFile: '<rootDir>/test/slow-setup.ts',
   collectCoverageFrom: ['lib/**/*.{js,ts}', '!**/node_modules/**', '!**/index.ts'],
   coverageReporters: ['text-summary', 'json'],
   globals: {
