28b06ce0101fd4c0c05c17f28e2fa7db9088a482
Rafael Oleza <rafeca@gmail.com>
2020-06-26 14:53:23 +0200

    Add toHaveGitError() custom matcher for tests

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,4 +1,4 @@
-import { GitProcess, IGitResult } from '../lib'
+import { GitProcess, IGitResult, GitError } from '../lib'
 
 // NOTE: bump these versions to the latest stable releases
 export const gitVersion = '2.26.'
@@ -51,5 +51,62 @@ export function verify(result: IGitResult, callback: (result: IGitResult) => voi
     console.log(` - stderr: ${result.stderr.trim()}`)
     console.log()
     throw e
+  }
+}
+
+/**
+ * Reverse maps the provided GitError to print a friendly name for debugging tests.
+ *
+ * @param gitError GitError
+ */
+function getFriendlyGitError(gitError: GitError): string {
+  const found = Object.entries(GitError).find(([key, value]) => {
+    return value === gitError
+  })
+
+  if (found === undefined) {
+    return gitError.toString()
+  }
+
+  return found[0]
+}
+
+expect.extend({
+  toHaveGitError(result: IGitResult, expectedError: GitError) {
+    let gitError = GitProcess.parseError(result.stderr)
+    if (gitError === null) {
+      gitError = GitProcess.parseError(result.stdout)
+    }
+
+    const message = () => {
+      return [
+        this.utils.matcherHint(`${this.isNot ? '.not' : ''}.toHaveGitError`, 'result', 'gitError'),
+        '',
+        'Expected',
+        `  ${this.utils.printExpected(getFriendlyGitError(expectedError))}`,
+        'Received:',
+        `  ${this.utils.printReceived(gitError ? getFriendlyGitError(gitError) : null)}`
+      ].join('\n')
+    }
+
+    if (gitError === expectedError) {
+      return {
+        pass: true,
+        message
+      }
+    }
+
+    return {
+      pass: false,
+      message
+    }
+  }
+})
+
+declare global {
+  namespace jest {
+    interface Matchers<R = IGitResult> {
+      toHaveGitError(result: GitError): CustomMatcherResult
+    }
   }
 }
