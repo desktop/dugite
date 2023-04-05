15d2df8fc4e392005342d0a3c7e0fddf13a1e42e
Maifee Ul Asad <maifeeulasad@gmail.com>
2023-04-05 21:07:58 +0600

    restore pipeline to original one; node-gyp install, configure, build from script, only runs on windows on post-install

---
+++ script/node-gyp.js
@@ -0,0 +1,44 @@
+const { exec } = require('child_process')
+const os = require('os')
+
+function runCommand(command) {
+  return new Promise((resolve, reject) => {
+    const task = exec(command)
+
+    task.stdout.on('data', data => {
+      console.log(`stdout: ${data}`)
+    })
+
+    task.stderr.on('data', data => {
+      console.error(`stderr: ${data}`)
+    })
+
+    task.on('close', code => {
+      console.log(`${command} exited with code ${code}`)
+      if (code === 0) {
+        resolve()
+      } else {
+        reject(new Error(`${command} exited with code ${code}`))
+      }
+    })
+  })
+}
+
+function installNodeGyp() {
+  return runCommand('npm i -g node-gyp@9.3.1')
+}
+
+function configureNodeGyp() {
+  return runCommand('node-gyp configure')
+}
+
+function buildNodeGyp() {
+  return runCommand('node-gyp build')
+}
+
+if (os.platform === 'win32') {
+  installNodeGyp()
+    .then(() => configureNodeGyp())
+    .then(() => buildNodeGyp())
+    .catch(err => console.error(err))
+}
