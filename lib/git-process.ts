6081edff8ab3eb0874f2f667cb9f26a346bbde28
Sergio Padrino <sergio.padrino@gmail.com>
2024-03-13 13:52:23 +0100

    Add parseBadConfigValueErrorInfo function

--- lib/git-process.ts
+++ lib/git-process.ts
@@ -291,6 +291,30 @@ export class GitProcess {
 
     return null
   }
+
+  public static parseBadConfigValueErrorInfo(
+    stderr: string
+  ): { key: string; value: string } | null {
+    const errorEntry = Object.entries(GitErrorRegexes).find(
+      ([_, v]) => v === GitError.BadConfigValue
+    )
+
+    if (errorEntry === undefined) {
+      return null
+    }
+
+    const m = stderr.match(errorEntry[0])
+
+    if (m === null) {
+      return null
+    }
+
+    if (!m[1] || !m[2]) {
+      return null
+    }
+
+    return { key: m[2], value: m[1] }
+  }
 }
 
 /**
