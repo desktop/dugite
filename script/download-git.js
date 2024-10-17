c99b188efb8e013897db3c33ded0f70487a61674
Markus Olsson <j.markus.olsson@gmail.com>
2024-10-17 13:16:59 +0200

    Less sync is good for u

--- script/download-git.js
+++ script/download-git.js
@@ -1,40 +1,50 @@
 const ProgressBar = require('progress')
 const tar = require('tar')
-const https = require('https')
 const { createHash } = require('crypto')
-const {
-  rm,
-  rmSync,
-  mkdir,
-  createReadStream,
-  createWriteStream,
-  existsSync,
-} = require('fs')
+const { createReadStream, createWriteStream } = require('fs')
+
+const { rm, mkdir, access } = require('fs/promises')
+
+/**
+ * Returns a value indicating whether or not the provided path exists (as in
+ * whether it's visible to the current process or not).
+ */
+const pathExists = path =>
+  access(path).then(
+    () => true,
+    () => false
+  )
+
 const { Readable } = require('stream')
 
 const config = require('./config')()
 
 const verifyFile = function (file, callback) {
-  const h = createHash('sha256').on('finish', () => {
-    const hash = h.digest('hex')
-    const match = hash === config.checksum
-    if (!match) {
-      console.log(
-        `Validation failed. Expected '${config.checksum}' but got '${hash}'`
-      )
-    }
-    callback(match)
-  })
+  return new Promise((resolve, reject) => {
+    const h = createHash('sha256')
+      .on('error', reject)
+      .on('finish', () => {
+        const hash = h.digest('hex')
+        if (hash !== config.checksum) {
+          reject(
+            new Error(
+              `Validation failed. Expected '${config.checksum}' but got '${hash}'`
+            )
+          )
+        } else {
+          resolve()
+        }
+      })
 
-  createReadStream(file).pipe(h)
+    createReadStream(file).pipe(h).on('error', reject)
+  })
 }
 
-const unpackFile = function (file) {
+const unpackFile = file => 
   tar.x({ cwd: config.outputPath, file }).catch(e => {
-    console.log('Unable to extract archive, aborting...', error)
+    console.log('Unable to extract archive, aborting...', e)
     process.exit(1)
   })
-}
 
 const downloadAndUnpack = async url => {
   const res = await fetch(url, {
@@ -42,7 +52,6 @@ const downloadAndUnpack = async url => {
       Accept: 'application/octet-stream',
       'User-Agent': 'dugite',
     },
-    secureProtocol: 'TLSv1_2_method',
   }).catch(e => {
     console.log('Unable to download archive, aborting...', e)
     process.exit(1)
@@ -62,59 +71,47 @@ const downloadAndUnpack = async url => {
     total: len,
   })
 
-  Readable.fromWeb(res.body)
-    .on('data', c => bar.tick(c.length))
-    .pipe(createWriteStream(config.tempFile))
-    .on('end', () => {
-      verifyFile(config.tempFile, valid => {
-        if (valid) {
-          unpackFile(config.tempFile)
-        } else {
-          console.log(`checksum verification failed, refusing to unpack...`)
-          process.exit(1)
-        }
-      })
-    })
-
-  // res.on('data', c => bar.tick(c.length))
+  await new Promise((resolve, reject) => {
+    Readable.fromWeb(res.body)
+      .on('data', c => bar.tick(c.length))
+      .pipe(createWriteStream(config.tempFile))
+      .on('error', reject)
+      .on('finish', resolve)
+  })
+  await verifyFile(config.tempFile)
+  await unpackFile(config.tempFile)
 }
 
-if (config.source === '') {
-  console.log(
-    `Skipping downloading embedded Git as platform '${process.platform}' is not supported.`
-  )
-  console.log(
-    `To learn more about using dugite with a system Git: https://git.io/vF5oj`
-  )
-  process.exit(0)
-}
-
-rm(config.outputPath, { recursive: true, force: true }, error => {
-  if (error) {
-    console.log(`Unable to clean directory at ${config.outputPath}`, error)
-    process.exit(1)
+;(async function run() {
+  if (config.source === '') {
+    console.log(
+      `Skipping downloading embedded Git as platform '${process.platform}' is not supported.`
+    )
+    console.log(
+      `To learn more about using dugite with a system Git: https://git.io/vF5oj`
+    )
+    process.exit(0)
   }
 
-  mkdir(config.outputPath, { recursive: true }, function (error) {
-    if (error) {
-      console.log(`Unable to create directory at ${config.outputPath}`, error)
-      process.exit(1)
-    }
-
-    const tempFile = config.tempFile
-
-    if (existsSync(tempFile)) {
-      verifyFile(tempFile, valid => {
-        if (valid) {
-          unpackFile(tempFile)
-        } else {
-          rmSync(tempFile)
-          downloadAndUnpack(config.source)
-        }
-      })
-      return
-    }
-
-    downloadAndUnpack(config.source)
+  await rm(config.outputPath, { recursive: true, force: true }).catch(error => {
+    console.log(`Unable to clean directory at ${config.outputPath}`, error)
+    process.exit(1)
   })
-})
+
+  await mkdir(config.outputPath, { recursive: true }).catch(error => {
+    console.log(`Unable to create directory at ${config.outputPath}`, error)
+    process.exit(1)
+  })
+
+  const tempFile = config.tempFile
+
+  if (await pathExists(tempFile)) {
+    await verifyFile(tempFile).catch(e => {
+      console.log('Unable to verify cached archive, removing...', e)
+      return rm(tempFile)
+    })
+    await unpackFile(tempFile)
+  } else {
+    await downloadAndUnpack(config.source)
+  }
+})()
