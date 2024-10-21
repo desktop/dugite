89a27600459eb67d730c1de052fd97526bb879ea
Markus Olsson <j.markus.olsson@gmail.com>
2024-10-21 16:08:50 +0200

    Fix example imports

--- examples/parsing-errors.ts
+++ examples/parsing-errors.ts
@@ -1,12 +1,12 @@
-import { GitProcess, GitError } from '../lib/'
+import { exec, GitError, parseError } from '../lib/'
 
 async function getError() {
   const branch = 'master'
   const path = 'C:/path/to/repo/'
 
-  const result = await GitProcess.exec(['pull', 'origin', branch], path)
+  const result = await exec(['pull', 'origin', branch], path)
   if (result.exitCode !== 0) {
-    const error = GitProcess.parseError(result.stderr)
+    const error = parseError(result.stderr)
     if (error) {
       if (error === GitError.HTTPSAuthenticationFailed) {
         // invalid credentials
