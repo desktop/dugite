a0f433922ad6dbf159b7193edc4232910239ea95
Markus Olsson <j.markus.olsson@gmail.com>
2024-11-20 15:03:00 +0100

    All tests are pretty fast nowadays, let's skip this complication

--- script/test.mjs
+++ script/test.mjs
@@ -2,20 +2,10 @@ import { spawn } from 'child_process'
 import { dirname, resolve, join } from 'path'
 import { fileURLToPath } from 'url'
 import { readdir } from 'fs/promises'
-
-if (process.argv.some(arg => ['-h', '--help'].includes(arg))) {
-  console.log(`Usage: ${process.argv0} [kind]`)
-  console.log(
-    '  kind: The kind of tests to run (e.g. "fast", "slow", "external", "all")'
+;(async function () {
+  const files = await readdir('test', { recursive: true }).then(x =>
+    x.filter(f => f.endsWith('-test.ts')).map(f => join('test', f))
   )
-  process.exit(0)
-}
-
-;(async function (kind) {
-  kind ??= 'all'
-  const files = await readdir(join('test', kind === 'all' ? '.' : kind), {
-    recursive: true,
-  }).then(x => x.filter(f => f.endsWith('-test.ts')).map(f => join('test', f)))
 
   const reporterDestinationArgs = ['--test-reporter-destination', 'stdout']
   const specTestReporterArgs = [
@@ -43,4 +33,4 @@ if (process.argv.some(arg => ['-h', '--help'].includes(arg))) {
       ),
     },
   }).on('exit', process.exit)
-})(process.argv[2])
+})()
