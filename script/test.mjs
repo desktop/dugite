0a12822c190900cd7723a95315684f4ffd6daa5f
Markus Olsson <j.markus.olsson@gmail.com>
2024-11-20 15:03:43 +0100

    Praise top level await!

--- script/test.mjs
+++ script/test.mjs
@@ -2,35 +2,34 @@ import { spawn } from 'child_process'
 import { dirname, resolve, join } from 'path'
 import { fileURLToPath } from 'url'
 import { readdir } from 'fs/promises'
-;(async function () {
-  const files = await readdir('test', { recursive: true }).then(x =>
-    x.filter(f => f.endsWith('-test.ts')).map(f => join('test', f))
-  )
 
-  const reporterDestinationArgs = ['--test-reporter-destination', 'stdout']
-  const specTestReporterArgs = [
-    '--test-reporter',
-    'spec',
-    ...reporterDestinationArgs,
-  ]
+const files = await readdir('test', { recursive: true }).then(x =>
+  x.filter(f => f.endsWith('-test.ts')).map(f => join('test', f))
+)
 
-  const testReporterArgs = process.env.GITHUB_ACTIONS
-    ? [
-        '--test-reporter',
-        'node-test-github-reporter',
-        ...reporterDestinationArgs,
-        ...specTestReporterArgs,
-      ]
-    : specTestReporterArgs
+const reporterDestinationArgs = ['--test-reporter-destination', 'stdout']
+const specTestReporterArgs = [
+  '--test-reporter',
+  'spec',
+  ...reporterDestinationArgs,
+]
 
-  spawn('node', ['--import', 'tsx', ...testReporterArgs, '--test', ...files], {
-    stdio: 'inherit',
-    env: {
-      ...process.env,
-      LOCAL_GIT_DIRECTORY: resolve(
-        dirname(fileURLToPath(import.meta.url)),
-        '../git/'
-      ),
-    },
-  }).on('exit', process.exit)
-})()
+const testReporterArgs = process.env.GITHUB_ACTIONS
+  ? [
+      '--test-reporter',
+      'node-test-github-reporter',
+      ...reporterDestinationArgs,
+      ...specTestReporterArgs,
+    ]
+  : specTestReporterArgs
+
+spawn('node', ['--import', 'tsx', ...testReporterArgs, '--test', ...files], {
+  stdio: 'inherit',
+  env: {
+    ...process.env,
+    LOCAL_GIT_DIRECTORY: resolve(
+      dirname(fileURLToPath(import.meta.url)),
+      '../git/'
+    ),
+  },
+}).on('exit', process.exit)
