d38c2064e45f2de58ba63f2dad33f19e8876b230
Markus Olsson <j.markus.olsson@gmail.com>
2024-11-20 15:11:08 +0100

    We already require this script to be run from the root

--- script/test.mjs
+++ script/test.mjs
@@ -1,6 +1,4 @@
 import { spawn } from 'child_process'
-import { dirname, resolve, join } from 'path'
-import { fileURLToPath } from 'url'
 import { readdir } from 'fs/promises'
 
 const files = await readdir('test', { recursive: true }).then(x =>
@@ -23,8 +21,7 @@ const testReporterArgs = process.env.GITHUB_ACTIONS
     ]
   : specTestReporterArgs
 
-const scriptsDir = dirname(fileURLToPath(import.meta.url))
-process.env.LOCAL_GIT_DIRECTORY = resolve(scriptsDir, '../git/')
+process.env.LOCAL_GIT_DIRECTORY = 'git'
 const args = ['--import', 'tsx', ...testReporterArgs, '--test', ...files]
 
 spawn('node', args, { stdio: 'inherit' }).on('exit', process.exit)
