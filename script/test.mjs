1a28fb4b2babe9bf770976336fa475f82ac5f607
Markus Olsson <j.markus.olsson@gmail.com>
2024-11-20 15:19:21 +0100

    The minification continues

--- script/test.mjs
+++ script/test.mjs
@@ -2,27 +2,22 @@ import { spawn } from 'child_process'
 import { join } from 'path'
 import { readdir } from 'fs/promises'
 
+function reporter(r) {
+  return ['--test-reporter', r, '--test-reporter-destination', 'stdout']
+}
+
 const files = await readdir('test', { recursive: true }).then(x =>
   x.filter(f => f.endsWith('-test.ts')).map(f => join('test', f))
 )
 
-const reporterDestinationArgs = ['--test-reporter-destination', 'stdout']
-const specTestReporterArgs = [
-  '--test-reporter',
-  'spec',
-  ...reporterDestinationArgs,
-]
-
-const testReporterArgs = process.env.GITHUB_ACTIONS
-  ? [
-      '--test-reporter',
-      'node-test-github-reporter',
-      ...reporterDestinationArgs,
-      ...specTestReporterArgs,
-    ]
-  : specTestReporterArgs
-
 process.env.LOCAL_GIT_DIRECTORY = 'git'
-const args = ['--import', 'tsx', ...testReporterArgs, '--test', ...files]
+
+const args = [
+  ...['--import', 'tsx'],
+  '--test',
+  ...reporter('spec'),
+  ...(process.env.GITHUB_ACTIONS ? reporter('node-test-github-reporter') : []),
+  ...files,
+]
 
 spawn('node', args, { stdio: 'inherit' }).on('exit', process.exit)
