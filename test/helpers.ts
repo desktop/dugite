a5d4334bd417a883d279775aee84e01646ece396
Markus Olsson <j.markus.olsson@gmail.com>
2025-05-19 13:51:31 +0200

    Update Git versions in test helpers
    Bumped `gitVersion` to 2.49.0 and `gitForWindowsVersion` to 2.49.0.windows.1 to reflect the latest stable releases.

--- test/helpers.ts
+++ test/helpers.ts
@@ -6,8 +6,8 @@ import { join } from 'path'
 import { tmpdir } from 'os'
 
 // NOTE: bump these versions to the latest stable releases
-export const gitVersion = '2.47.1'
-export const gitForWindowsVersion = '2.47.1.windows.2'
+export const gitVersion = '2.49.0'
+export const gitForWindowsVersion = '2.49.0.windows.1'
 export const gitLfsVersion = '3.6.1'
 export const gitCredentialManagerVersion = '2.6.1'
 
